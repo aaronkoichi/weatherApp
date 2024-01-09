38dd68aa002f9a99607b83bc270264edba1d2b21
aaronkoichi <aaronlee4399@gmail.com>
2024-01-09 16:17:47 +0800

    Added JS code for getting weather details from API (API key removed in code)

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
- 
+function getWeather(cityID) {
+	let key = "";
+	fetch(
+		"https://api.openweathermap.org/data/2.5/weather?id" +
+		cityID +
+		"&appID" +
+		key,
+	)
+		.then(function(resp) {
+			return resp.json();
+		})
+		.then(function(data) {
+			// console.log(data);
+			writeWeather(data);
+		});
+}
+
+function writeWeather(data) {
+	let celcius = Math.round(parseFloat(data.main.temp) - 273.15);
+
+	document.getElementById("description").innerHTML =
+		data.weather[0].description;
+	document.getElementById("degree").innerHTML = celcius + "&deg;";
+	document.getElementById("country").innerHTML = data.name;
+}
+
+window.onload = function() {
+	getWeather(1733046);
+};
